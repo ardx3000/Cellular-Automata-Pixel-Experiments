df6bd3011a805872ea3727666b72d926157940f8
Abdulla Y. Daniel <daniel.ardx@gmail.com>
2024-06-01 19:18:32 +0100

    Init
    Simple sand simulation Will extend later for more particle

--- CA Simulation/compile.sh
+++ CA Simulation/compile.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 # Compile all .c files in the current directory
-gcc -o OpenGLExample *.c \
-    -I/opt/homebrew/Cellar/glew/2.2.0_1/include \
-    -I/opt/homebrew/Cellar/glfw/3.4/include \
-    -L/opt/homebrew/Cellar/glew/2.2.0_1/lib \
-    -lGLEW \
-    -L/opt/homebrew/Cellar/glfw/3.4/lib \
-    -lglfw \
-    -framework OpenGL
+gcc -o CAAp main.c -I/opt/homebrew/include -L/opt/homebrew/lib -lraylib -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL
--- CA Simulation/main.c
+++ CA Simulation/main.c
@@ -1,56 +1,129 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <stdio.h>
+#include "raylib.h"
+#include <stdbool.h>
+#include <stdlib.h>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+#define CELL_SIZE 15  // Increase this value to make the sand particles bigger
+
+typedef struct Cell {
+    bool hasSand;
+} Cell;
+
+void InitializeGrid(Cell grid[SCREEN_HEIGHT / CELL_SIZE][SCREEN_WIDTH / CELL_SIZE]);
+void UpdateGrid(Cell grid[SCREEN_HEIGHT / CELL_SIZE][SCREEN_WIDTH / CELL_SIZE]);
+void DrawSandGrid(Cell grid[SCREEN_HEIGHT / CELL_SIZE][SCREEN_WIDTH / CELL_SIZE]);
+void DropSand(Cell grid[SCREEN_HEIGHT / CELL_SIZE][SCREEN_WIDTH / CELL_SIZE], int mouseX, int mouseY);
+
+int main(void)
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cellular Automaton - Sand Simulation");
+
+    const int rows = SCREEN_HEIGHT / CELL_SIZE;
+    const int cols = SCREEN_WIDTH / CELL_SIZE;
+
+    Cell grid[rows][cols];
+    InitializeGrid(grid);
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        // Update
+        UpdateGrid(grid);
+
+        // Check for mouse input to drop sand
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+        {
+            int mouseX = GetMouseX();
+            int mouseY = GetMouseY();
+            DropSand(grid, mouseX, mouseY);
+        }
+
+        // Draw
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawSandGrid(grid);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
 
-int main() {
-    // Initialize GLFW
-    if (!glfwInit()) {
-        fprintf(stderr, "Failed to initialize GLFW\n");
-        return -1;
+void InitializeGrid(Cell grid[SCREEN_HEIGHT / CELL_SIZE][SCREEN_WIDTH / CELL_SIZE])
+{
+    // Initialize all cells to false
+    for (int i = 0; i < SCREEN_HEIGHT / CELL_SIZE; i++)
+    {
+        for (int j = 0; j < SCREEN_WIDTH / CELL_SIZE; j++)
+        {
+            grid[i][j].hasSand = false;
+        }
     }
+}
 
-    // Set OpenGL version to 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
-    if (!window) {
-        fprintf(stderr, "Failed to create GLFW window\n");
-        glfwTerminate();
-        return -1;
+void UpdateGrid(Cell grid[SCREEN_HEIGHT / CELL_SIZE][SCREEN_WIDTH / CELL_SIZE])
+{
+    // Iterate through each cell
+    for (int i = SCREEN_HEIGHT / CELL_SIZE - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < SCREEN_WIDTH / CELL_SIZE; j++)
+        {
+            if (grid[i][j].hasSand)
+            {
+                // Check if the cell below is empty and within bounds
+                if (i + 1 < SCREEN_HEIGHT / CELL_SIZE && !grid[i + 1][j].hasSand)
+                {
+                    // Move the sand particle down
+                    grid[i][j].hasSand = false;
+                    grid[i + 1][j].hasSand = true;
+                }
+                else 
+                {
+                    //Check if sand can fall left
+                    if(j > 0 && !grid[i][j - 1].hasSand && !grid[i + 1][j - 1].hasSand)
+                    {
+                        grid[i][j].hasSand = false;
+                        grid[i + 1][j - 1].hasSand = true;
+                    }
+                    //Check if sand can fall right
+                    else if( j < SCREEN_WIDTH / CELL_SIZE - 1 && !grid[i][j +1].hasSand && !grid[i + 1][j + 1].hasSand)
+                    {
+                        grid[i][j].hasSand = false;
+                        grid[i + 1][j + 1].hasSand = true;
+                    }
+                }
+            }
+        }
     }
+}
 
-    // Make the window's context current
-    glfwMakeContextCurrent(window);
-
-    // Initialize GLEW
-    if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "Failed to initialize GLEW\n");
-        return -1;
+void DrawSandGrid(Cell grid[SCREEN_HEIGHT / CELL_SIZE][SCREEN_WIDTH / CELL_SIZE])
+{
+    for (int i = 0; i < SCREEN_HEIGHT / CELL_SIZE; i++)
+    {
+        for (int j = 0; j < SCREEN_WIDTH / CELL_SIZE; j++)
+        {
+            if (grid[i][j].hasSand)
+            {
+                // Draw a rectangle for each sand particle
+                DrawRectangle(j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE, BROWN);
+            }
+        }
     }
+}
 
-    // Set the viewport size
-    glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+void DropSand(Cell grid[SCREEN_HEIGHT / CELL_SIZE][SCREEN_WIDTH / CELL_SIZE], int mouseX, int mouseY)
+{
+    // Convert mouse coordinates to grid indices
+    int gridX = mouseX / CELL_SIZE;
+    int gridY = mouseY / CELL_SIZE;
 
-    // Main loop
-    while (!glfwWindowShouldClose(window)) {
-        // Render here
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Swap front and back buffers
-        glfwSwapBuffers(window);
-
-        // Poll for and process events
-        glfwPollEvents();
+    // Drop sand at the clicked position
+    if (gridX >= 0 && gridX < SCREEN_WIDTH / CELL_SIZE && gridY >= 0 && gridY < SCREEN_HEIGHT / CELL_SIZE)
+    {
+        grid[gridY][gridX].hasSand = true;
     }
-
-    glfwTerminate();
-    return 0;
 }
